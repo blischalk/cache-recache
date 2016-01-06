a23a312889828b58a5863d7304f9228f4310142f
Brett Lischalk <blischalk@opinionlab.com>
2016-01-06 08:31:20 -0600

    Add some specs

--- spec/cache_recache/core_spec.clj
+++ spec/cache_recache/core_spec.clj
@@ -1,6 +1,7 @@
 (ns cache-recache.core-spec
-  (:require [speclj.core :refer :all]
-            [cache-recache.core :refer :all])
+  (:require [cache-recache.core :refer :all]
+            [clojure.core.async :refer :all]
+            [speclj.core :refer :all])
   (:import [cache_recache.core CacheStrategy]))
 
 
@@ -10,9 +11,29 @@
   (CacheStrategy. "instruments"
                    (fn [] basic-test-data)
                    #(println "hello world.")
-                   60))
+                   1))
 
 (describe "cache-recache"
   (it "returns a copy of cached data"
-    (let [[cached ch] (cache-recache basic-strategy)]
-      (should= cached basic-test-data))))
+    (-> basic-strategy
+        cache-recache
+        :data
+        (should= basic-test-data)))
+
+  (it "returns a channel that acts like a channel"
+    (-> basic-strategy
+        cache-recache
+        :channel
+        close!
+        should-not-throw))
+
+  (it "checks cache validity every n interval"
+    (let [call-counter (atom 0)
+          strategy (CacheStrategy. "foobar"
+                                   (fn [] (swap! call-counter inc))
+                                   (fn [] )
+                                   1)]
+      (Thread/sleep 5000)
+      (should (> @call-counter 1))))
+
+  (it "notifies on channel a cache refresh"))
--- src/cache_recache/core.clj
+++ src/cache_recache/core.clj
@@ -1,12 +1,23 @@
-(ns cache-recache.core)
+(ns cache-recache.core
+  (:require [clojure.core.async :refer :all]
+            [chime :refer [chime-ch]]
+            [clj-time.core :as t]))
 
-(def ^:private cache (atom {}))
+(def ^:private cache-store (atom {}))
 
+(defrecord Cache [data channel])
 (defrecord CacheStrategy [name populate on-invalidate freq-mins])
 
 (defn cache-recache
   "Sets up a cache and polling to re-cache"
   [strategy]
-  (let [data ((:populate strategy))]
-    (swap! cache assoc (:name strategy) data)
-    [data :foo]))
+  (let [data ((:populate strategy))
+        ch (chan)
+        cache (Cache. data ch)
+        chimes (chime-ch times {:ch (a/chan (a/sliding-buffer 1))})]
+    (swap! cache-store assoc (:name strategy) cache)
+    (go-loop []
+      (when-let [time (<! chimes)]
+        ;; ...
+        (recur)))
+    cache))
